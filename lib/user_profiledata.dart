98fb777948e11b832903e52857436bf134fa187d
Ankit <ankitksharma07@gmail.com>
2021-05-14 11:28:04 +0530

    updated userdata

--- lib/user_profiledata.dart
+++ lib/user_profiledata.dart
@@ -15,73 +15,25 @@ class _UserProfileState extends State<UserProfile> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('User Profile')),
-      // ignore: sized_box_for_whitespace
-      body: Container(
-        height: 150,
-        width: 420,
-        child: Card(
-          child: Stack(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Text(
-                  'User_Name: ${widget.value.name}',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 17),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40),
-                child: Text(
-                  'User_Mail_ID : ${widget.value.email}',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 17),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 70),
-                child: Text(
-                  'Phone_Number : ${widget.value.phone}',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 17),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 100),
-                child: Text(
-                  'Address[Geo] : lng:-${widget.value.lng} lat:-${widget.value.lat}',
-                  style: const TextStyle(
-                      fontWeight: FontWeight.bold, fontSize: 17),
-                ),
-              ),
-              Positioned(
-                left: 355,
-                child: IconButton(
-                  tooltip: "Edit Profile",
-                  icon: const Icon(
-                    Icons.edit,
-                  ),
-                  onPressed: () {},
-                ),
-              )
-            ],
+      body: Card(
+        elevation: 3,
+        child: ListTile(
+          title: Text(
+            'User_Name: ${widget.value.name}'
+            '\nEmail: ${widget.value.email}'
+            '\nNumber:${widget.value.phone}'
+            '\nAddress[Geo]:'
+            '\nlat:- ${widget.value.lat}'
+            '\nlng:-${widget.value.lng}',
+            style: const TextStyle(color: Colors.lightBlue),
           ),
+          trailing: IconButton(
+              icon: const Icon(
+                Icons.edit,
+                color: Colors.lightBlue,
+              ),
+              onPressed: () {}),
         ),
-      ),
-    );
-  }
-}
-class UpdateUserProfile extends StatefulWidget {
-  @override
-  _UpdateUserProfileState createState() => _UpdateUserProfileState();
-}
-
-class _UpdateUserProfileState extends State<UpdateUserProfile> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:const Text("Update Profile"),
       ),
     );
   }
